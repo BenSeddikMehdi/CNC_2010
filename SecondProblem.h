2d218f6828f0c2892d1152242c590e141e7037e2
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-08 21:58:13 +0100

    Processing the question 1

--- SecondProblem.h
+++ SecondProblem.h
@@ -5,6 +5,41 @@
 #ifndef CNC_2010_SECONDPROBLEM_H
 #define CNC_2010_SECONDPROBLEM_H
 
+typedef struct {
+    int id;
+    char clientName[20];
+    float price;
+    int deadline;
+}bell_t;
+
+typedef struct list {
+    bell_t info;
+    struct list *next;
+}list_t;
+
+const int currentYear = 2010;
+list_t *first_element;
+
+/**************/
+/* Question 1 */
+int do_get_number_of_elements(int (*op) (list_t*), list_t* pList) {
+    return op(pList);
+}
+int get_number_of_elements(list_t* pList) {
+    int i = 0;
+    while (pList != NULL) {
+        i++;
+        pList = pList->next;
+    }
+    return i;
+}
+
+/**************/
+/* Question 2 */
+
+
+
+
 
 
 #endif //CNC_2010_SECONDPROBLEM_H
