4f8dc13efa5671c704e727a81308947ac9325173
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-08 01:39:54 +0100

    Processing the question 1

--- FirstProblem.h
+++ FirstProblem.h
@@ -5,7 +5,31 @@
 #ifndef CNC_2010_FIRSTPROBLEM_H
 #define CNC_2010_FIRSTPROBLEM_H
 
+#include <string.h>
+/****************************************/
+/* Problem I: Zipping and RLE Algorithm */
+      /* RLE: Run-Length Encoding */
+/****************************************/
 
+/**************/
+/* Question 1 */
+void do_string_zipping(void (*op) (char*, char []), char* string, char temp[]) {
+    op(string, temp);
+}
+void string_zipping(char* str, char* temp) {
+    int j = 0;
+    const unsigned int n =  strlen(str);
+    unsigned int a = 0;
+    a = n;
+    for (int k = 0; k < (int)(n); ++k) {
+        if (str[k] == str[k+1]) continue;
+        else {
+            temp[j] = str[k];
+            j++;
+        }
+    }
+    temp[j] = '\0';
+}
 
 
 
--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include "FirstProblem.h"
 
 int main() {
+    char zippedString[] = "";
+    //test = do_string_zipping(string_zipping, "AAAXXRRRRRTT", temp);
+    do_string_zipping(string_zipping, "AAAXX    EKKEV 786888", zippedString);
+    printf("Zipped String = %s", zippedString);
 
     return 0;
 }
