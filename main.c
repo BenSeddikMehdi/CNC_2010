242a99c75f24d505c04c7cce671b467bdbb47778
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-08 01:41:19 +0100

    End of the question 1

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int main() {
     char zippedString[] = "";
     //test = do_string_zipping(string_zipping, "AAAXXRRRRRTT", temp);
-    do_string_zipping(string_zipping, "AAAXX    EKKEV 786888", zippedString);
+    do_string_zipping(string_zipping, "AAAXXX RRKKEV 786888", zippedString);
     printf("Zipped String = %s", zippedString);
 
     return 0;
