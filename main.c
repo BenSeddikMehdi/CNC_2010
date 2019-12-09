f3378086401d20ad0edb1645229a5451f5088adb
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-09 22:14:07 +0100

    End of the question 4

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int main() {
     list_t* temp = NULL;
     temp = do_create_linked_list(create_linked_list, temp);
-    //temp = do_delete_element(delete_element, temp, 6);
+    temp = do_delete_element(delete_element, temp, 4);
     temp = do_bell_taxes(bell_taxes, temp);
     printLinkedList(temp);
     temp = NULL;
