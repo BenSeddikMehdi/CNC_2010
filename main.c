c274100187492e8c65546fbd1315dd07f702f934
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-18 23:15:11 +0100

    End of the question 2-1 of the first problem

--- main.c
+++ main.c
@@ -3,8 +3,6 @@
 #include "SecondProblem.h"
 
 int main() {
-    list_t* plist = NULL;
-    plist = do_create_linked_list(create_linked_list, plist);
-    do_delete_element(delete_element, plist, 3);
+
     return 0;
 }
