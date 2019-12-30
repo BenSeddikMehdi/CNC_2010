3a138caaeef84b041688c0967a7240e3db531c7c
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-30 15:48:32 +0100

    End of the question 2-1 of the first problem

--- main.c
+++ main.c
@@ -3,9 +3,11 @@
 #include "SecondProblem.h"
 
 int main() {
-    char str[] = "abbrevGGG";
-    char temp[] = "";
-    do_string_zipping(string_zipping, str, temp);
-    printf("temp = %s\n", temp);
+    char str[] = "HHHJUFEFEE";
+    char temp[] = "", temp_2[] = "";
+    /*do_string_zipping(string_zipping, str, temp);
+    printf("temp = %s\n", temp);*/
+    /*do_string_coding(string_coding, str, temp_2);
+    printf("temp_2 = %s\n", temp_2);*/
     return 0;
 }
