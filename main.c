f5b83fa3067ecff0304331f935dfcfedfef0065c
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-30 15:50:17 +0100

    End of the question 2-1 of the first problem

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 #include "SecondProblem.h"
 
 int main() {
-    char str[] = "HHHJUFEFEE";
+    char str[] = "aabbbfffkkkr";
     char temp[] = "", temp_2[] = "";
-    /*do_string_zipping(string_zipping, str, temp);
-    printf("temp = %s\n", temp);*/
+    do_string_zipping(string_zipping, str, temp);
+    printf("temp = %s\n", temp);
     do_string_coding(string_coding, str, temp_2);
     printf("temp_2 = %s\n", temp_2);
     return 0;
